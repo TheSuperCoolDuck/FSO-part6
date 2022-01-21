a2805eee3443967d9b2e0cff0c00a200c12491eb
Daniel Chen <dchen0058@gmail.com>
2022-01-21 17:17:20 +1100

    completed 6.9

---
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,12 @@
+const notificationReducer = (state, action)=>{
+  switch(action.type){
+    case 'SET_NOTIFICATION':
+      return action.notification
+    case 'REMOVE_NOTIFICATION':
+      return ''
+    default:
+      return state
+  }
+}
+
+export default notificationReducer
